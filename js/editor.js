844041391ee1fd1e00541337331bf921aad828e9
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2021-12-14 14:22:55 -0300

    add script comunidade

--- js/editor.js
+++ js/editor.js
@@ -1,9 +1,14 @@
 let inputCor = document.getElementById('cor-selecionada')
 let bgCodigo = document.querySelector('#codigo .bg-codigo')
 let btnSalvar = document.getElementById('btn-salvar-projeto')
+let codigoProjeto = document.getElementById('input-codigo')
 let nomeProjeto = document.getElementById('nome-projeto')
 let descricaoProjeto = document.getElementById('descricao-projeto')
+let autor = document.getElementById('usuario')
+let fotoAutor = document.getElementById('foto-usuario')
 let linguagem = document.getElementById('linguagens')
+
+let listaProjetos
 
 inputCor.addEventListener('input', () => {
   let corSelecionada = inputCor.value
@@ -12,23 +17,28 @@ inputCor.addEventListener('input', () => {
 
 btnSalvar.addEventListener('click', () => {
   let infosProjeto = {
+    codigo: codigoProjeto.value,
     nome: nomeProjeto.value,
     descricao: descricaoProjeto.value,
+    autor: autor.textContent,
+    fotoAutor: fotoAutor.getAttribute('src'),
     linguagem: linguagem.value,
     corBg: inputCor.value
   }
 
-  localStorage.setItem('db_infosProjeto', JSON.stringify(infosProjeto))
+  listaProjetos.push(infosProjeto)
+
+  localStorage.setItem('db_infosProjeto', JSON.stringify(listaProjetos))
 
   // console.log(JSON.parse(localStorage.getItem('db_infosProjeto')))
+
+  location.reload()
 })
 
 if (localStorage.getItem('db_infosProjeto')) {
-  let projeto = JSON.parse(localStorage.getItem('db_infosProjeto'))
-
-  nomeProjeto.value = projeto.nome
-  descricaoProjeto.value = projeto.descricao
-  linguagem.value = projeto.linguagem
-  inputCor.value = projeto.corBg
-  bgCodigo.style.backgroundColor = projeto.corBg
+  listaProjetos = JSON.parse(localStorage.getItem('db_infosProjeto'))
+} else {
+  listaProjetos = []
 }
+
+localStorage.setItem('db_usuarioCorrente', autor.textContent)
